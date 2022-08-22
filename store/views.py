fe0eec83e3e771c636f8c529438780cea0722d33
Kenneth Luong <102771088+kenluong28@users.noreply.github.com>
2022-08-21 23:33:13 -0400

    links

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
@@ -15,3 +16,8 @@ def all_products(request):
 def product_detail(request,slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
     return render(request, 'store/products/detail.html', {'product': product})
+
+def category_list(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'store/products/category.html', {'category': category, 'products': products})
