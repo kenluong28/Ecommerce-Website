6f3689e50b2478aa3dba734c94c74bb3ceb189e4
Kenneth Luong <102771088+kenluong28@users.noreply.github.com>
2022-08-21 23:14:54 -0400

    product page (details)

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -29,6 +30,9 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
 
     def __str__(self):
         return self.title
