35a94bc16b58b0abb00048bd6a84e13217a5ec9e
Kenneth Luong <102771088+kenluong28@users.noreply.github.com>
2022-09-07 00:33:18 -0400

    cart delete functionality (cart update)

--- basket/views.py
+++ basket/views.py
@@ -29,8 +29,9 @@ def basket_delete(request):
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
         basket.delete(product=product_id)
+        basketqty = basket.__len__()
         baskettotal = basket.get_total_price()
-        response = JsonResponse({'subtotal': baskettotal})
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
         return response
 
 def basket_update(request):
