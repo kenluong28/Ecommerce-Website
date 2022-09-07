3162b4c3d42d26c544008e9b5f3b4a1cf7406d6e
Kenneth Luong <102771088+kenluong28@users.noreply.github.com>
2022-09-07 00:17:05 -0400

    cart update functionality

--- basket/basket.py
+++ basket/basket.py
@@ -62,5 +62,16 @@ class Basket():
 
         self.save()
 
+    def update(self, product, qty):
+        """
+        Update values in session data
+        """
+        product_id = str(product)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+
+        self.save()
+
     def save(self):
         self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,3 @@
-from ast import Delete
-from urllib import response
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 
@@ -32,4 +30,16 @@ def basket_delete(request):
         product_id = int(request.POST.get('productid'))
         basket.delete(product=product_id)
         response = JsonResponse({'Success': True})
+        return response
+
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        basket.update(product=product_id, qty=product_qty)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
         return response
